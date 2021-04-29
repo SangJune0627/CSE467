4b4eb220452d0ffceaf35a280ab4d69d39c8fe74
SangJune0627 <yain20301@gmail.com>
2021-04-29 21:11:34 +0900

    formatting

--- HW2/tests.c
+++ HW2/tests.c
@@ -7,14 +7,22 @@
 #include <unistd.h>
 #include "filter.h"
 
-struct image generate_rand_img() {
+struct image generate_rand_img()
+{
   struct image img;
-  do{  img.size_x = rand() % 256; } while(img.size_x == 0);
-  do{  img.size_y = rand() % 256; } while(img.size_y == 0);
+  do
+  {
+    img.size_x = rand() % 256;
+  } while (img.size_x == 0);
+  do
+  {
+    img.size_y = rand() % 256;
+  } while (img.size_y == 0);
   img.px = malloc(img.size_x * img.size_y * sizeof(struct pixel));
-  if(img.px == NULL)
+  if (img.px == NULL)
     assert(0 && "Rerun test, malloc failed");
-  for(long i = 0; i < img.size_y * img.size_x; i++) {
+  for (long i = 0; i < img.size_y * img.size_x; i++)
+  {
     img.px[i].red = rand();
     img.px[i].green = rand();
     img.px[i].blue = rand();
@@ -23,15 +31,17 @@ struct image generate_rand_img() {
 
   return img;
 }
-struct image duplicate_img(struct image img) {
+struct image duplicate_img(struct image img)
+{
   struct image img_dup;
 
   img_dup.size_x = img.size_x;
   img_dup.size_y = img.size_y;
   img_dup.px = malloc(img.size_x * img.size_y * sizeof(struct pixel));
-  if(img_dup.px == NULL)
+  if (img_dup.px == NULL)
     assert(0 && "Rerun test, malloc failed");
-  for(long i = 0; i < img.size_y * img.size_x; i++) {
+  for (long i = 0; i < img.size_y * img.size_x; i++)
+  {
     img_dup.px[i].red = img.px[i].red;
     img_dup.px[i].green = img.px[i].green;
     img_dup.px[i].blue = img.px[i].blue;
@@ -43,7 +53,8 @@ struct image duplicate_img(struct image img) {
 
 /* Grayscale function should not crash when the weights are 
  * at the limits of the double data size */
-START_TEST(grayscale_double_limit) {
+START_TEST(grayscale_double_limit)
+{
   srand(time(NULL) ^ getpid());
 
   /* Generate random png image */
@@ -51,12 +62,13 @@ START_TEST(grayscale_double_limit) {
 
   /* Limiting cases of double */
   double weight_limits[] = {DBL_MIN, DBL_TRUE_MIN, DBL_MAX, DBL_EPSILON, DBL_MIN_EXP};
-  int n_limits = sizeof(weight_limits)/sizeof(weight_limits[0]);
+  int n_limits = sizeof(weight_limits) / sizeof(weight_limits[0]);
 
   /* grayscale requires array of 3 weights */
-  for(unsigned i0 = 0; i0 < n_limits; i0++)
-    for(unsigned i1 = 0; i1 < n_limits; i1++)
-      for(unsigned i2 = 0; i2 < n_limits; i2++) {
+  for (unsigned i0 = 0; i0 < n_limits; i0++)
+    for (unsigned i1 = 0; i1 < n_limits; i1++)
+      for (unsigned i2 = 0; i2 < n_limits; i2++)
+      {
         double weights[3] = {weight_limits[i0], weight_limits[i1], weight_limits[i2]};
         /* Merely checking that the function does not crash */
         filter_grayscale(&img, weights);
@@ -67,7 +79,8 @@ START_TEST(grayscale_double_limit) {
 }
 END_TEST
 
-START_TEST(grayscale_functionality) {
+START_TEST(grayscale_functionality)
+{
   srand(time(NULL) ^ getpid());
 
   /* Generate random png image */
@@ -75,18 +88,18 @@ START_TEST(grayscale_functionality) {
   uint8_t rand_alpha = rand();
   double weights[] = {0, 0, 0};
   uint16_t sz_x = img.size_x, sz_y = img.size_y;
-  for(long i = 0; i < sz_y; i++)
-    for(long j = 0; j < sz_x; j++)
+  for (long i = 0; i < sz_y; i++)
+    for (long j = 0; j < sz_x; j++)
       img.px[i * sz_x + j].alpha = rand_alpha;
-
 
   filter_grayscale(&img, weights);
 
   ck_assert_uint_eq(img.size_x, sz_x);
   ck_assert_uint_eq(img.size_y, sz_y);
   ck_assert_ptr_ne(img.px, NULL);
-  for(long i = 0; i < sz_y; i++)
-    for(long j = 0; j < sz_x; j++){
+  for (long i = 0; i < sz_y; i++)
+    for (long j = 0; j < sz_x; j++)
+    {
       long idx = i * sz_x + j;
       ck_assert_uint_eq(img.px[idx].red, 0);
       ck_assert_uint_eq(img.px[idx].green, 0);
@@ -98,7 +111,8 @@ START_TEST(grayscale_functionality) {
 }
 END_TEST
 
-START_TEST(sepia_limits0) {
+START_TEST(sepia_limits0)
+{
   struct image img;
   img.size_x = 0;
   img.size_y = 0;
@@ -110,7 +124,8 @@ START_TEST(sepia_limits0) {
 }
 END_TEST
 
-START_TEST(sepia_limits_depth) {
+START_TEST(sepia_limits_depth)
+{
   srand(time(NULL) ^ getpid());
   struct image img;
   struct pixel pxl;
@@ -122,7 +137,7 @@ START_TEST(sepia_limits_depth) {
   pxl.alpha = 255;
 
   uint8_t depth = 255;
-  filter_sepia(&img, &depth); 
+  filter_sepia(&img, &depth);
   ck_assert_uint_eq(pxl.red, 255);
   ck_assert_uint_eq(pxl.green, 255);
   ck_assert_uint_eq(pxl.blue, 0);
@@ -130,7 +145,7 @@ START_TEST(sepia_limits_depth) {
 
   depth = 128;
   pxl.red = pxl.green = pxl.blue = 0;
-  filter_sepia(&img, &depth); 
+  filter_sepia(&img, &depth);
   ck_assert_uint_eq(pxl.red, 255);
   ck_assert_uint_eq(pxl.green, 128);
   ck_assert_uint_eq(pxl.blue, 0);
@@ -138,7 +153,7 @@ START_TEST(sepia_limits_depth) {
 
   depth = 0;
   uint8_t rand8 = pxl.red = pxl.green = pxl.blue = rand();
-  filter_sepia(&img, &depth); 
+  filter_sepia(&img, &depth);
   ck_assert_uint_eq(pxl.red, rand8);
   ck_assert_uint_eq(pxl.green, rand8);
   ck_assert_uint_eq(pxl.blue, rand8);
@@ -147,17 +162,16 @@ START_TEST(sepia_limits_depth) {
 END_TEST
 
 char *sepia_summers[] = {
-  "test_imgs/summer_sepia_0.png",
-  "test_imgs/summer_sepia_10.png",
-  "test_imgs/summer_sepia_20.png",
-  "test_imgs/summer_sepia_30.png",
-  "test_imgs/summer_sepia_42.png"
-};
+    "test_imgs/summer_sepia_0.png",
+    "test_imgs/summer_sepia_10.png",
+    "test_imgs/summer_sepia_20.png",
+    "test_imgs/summer_sepia_30.png",
+    "test_imgs/summer_sepia_42.png"};
 uint8_t sepia_depths[] = {
-  0x0, 0x10, 0x20, 0x30, 0x42
-};
+    0x0, 0x10, 0x20, 0x30, 0x42};
 
-START_TEST(sepia_example_image) {
+START_TEST(sepia_example_image)
+{
   struct image *img, *img_sepia, img_dup;
 
   ck_assert_int_eq(load_png("test_imgs/summer.png", &img), 0);
@@ -169,7 +183,8 @@ START_TEST(sepia_example_image) {
 
   ck_assert_uint_eq(img_sepia->size_x, img->size_x);
   ck_assert_uint_eq(img_sepia->size_y, img->size_y);
-  for(long j = 0; j < img->size_x * img->size_y; j++) {
+  for (long j = 0; j < img->size_x * img->size_y; j++)
+  {
     ck_assert_uint_eq(img_sepia->px[j].red, img->px[j].red);
     ck_assert_uint_eq(img_sepia->px[j].green, img->px[j].green);
     ck_assert_uint_eq(img_sepia->px[j].blue, img->px[j].blue);
@@ -183,8 +198,8 @@ START_TEST(sepia_example_image) {
 }
 END_TEST
 
-
-START_TEST(bw_limits0) {
+START_TEST(bw_limits0)
+{
   struct image img;
   img.size_x = 0;
   img.size_y = 0;
@@ -196,9 +211,10 @@ START_TEST(bw_limits0) {
 }
 END_TEST
 
-START_TEST(bw_threshold) {
+START_TEST(bw_threshold)
+{
   srand(time(NULL) ^ getpid());
-  
+
   struct pixel img_pxl, img_dup_pxl;
   struct image img = generate_rand_img();
   struct image img_dup = duplicate_img(img);
@@ -207,7 +223,8 @@ START_TEST(bw_threshold) {
   filter_bw(&img, &threshold);
   ck_assert_uint_eq(img.size_x, img_dup.size_x);
   ck_assert_uint_eq(img.size_y, img_dup.size_y);
-  for(long j = 0; j < img.size_x * img.size_y; j++) {
+  for (long j = 0; j < img.size_x * img.size_y; j++)
+  {
     img_pxl = img.px[j];
     img_dup_pxl = img_dup.px[j];
 
@@ -218,13 +235,16 @@ START_TEST(bw_threshold) {
     /* Check that alpha is unaffected */
     ck_assert_uint_eq(img_pxl.alpha, img_dup_pxl.alpha);
 
-    if(img_pxl.red == 0) {
+    if (img_pxl.red == 0)
+    {
       ck_assert_uint_lt(img_pxl.red, 3);
       ck_assert_uint_lt(img_pxl.green, 3);
       ck_assert_uint_lt(img_pxl.blue, 3);
       uint8_t total = img_pxl.red + img_pxl.green + img_pxl.blue;
       ck_assert_uint_lt(total, 3);
-    } else {
+    }
+    else
+    {
       ck_assert_uint_eq(img_pxl.red, 255);
       ck_assert_uint_eq(img_pxl.green, 255);
       ck_assert_uint_eq(img_pxl.blue, 255);
@@ -236,7 +256,8 @@ START_TEST(bw_threshold) {
   filter_bw(&img, &threshold);
   ck_assert_uint_eq(img.size_x, img_dup.size_x);
   ck_assert_uint_eq(img.size_y, img_dup.size_y);
-  for(long j = 0; j < img.size_x * img.size_y; j++) {
+  for (long j = 0; j < img.size_x * img.size_y; j++)
+  {
     img_pxl = img.px[j];
     img_dup_pxl = img_dup.px[j];
 
@@ -256,14 +277,14 @@ START_TEST(bw_threshold) {
 END_TEST
 
 char *bw_summers[] = {
-  "test_imgs/summer_bw_40.png",
-  "test_imgs/summer_bw_80.png",
+    "test_imgs/summer_bw_40.png",
+    "test_imgs/summer_bw_80.png",
 };
 uint8_t bw_thresholds[] = {
-  0x40, 0x80
-};
+    0x40, 0x80};
 
-START_TEST(bw_example_image) {
+START_TEST(bw_example_image)
+{
   struct image *img, *img_bw, img_dup;
 
   ck_assert_int_eq(load_png("test_imgs/summer.png", &img), 0);
@@ -275,7 +296,8 @@ START_TEST(bw_example_image) {
 
   ck_assert_uint_eq(img_bw->size_x, img->size_x);
   ck_assert_uint_eq(img_bw->size_y, img->size_y);
-  for(long j = 0; j < img->size_x * img->size_y; j++) {
+  for (long j = 0; j < img->size_x * img->size_y; j++)
+  {
     ck_assert_uint_eq(img_bw->px[j].red, img->px[j].red);
     ck_assert_uint_eq(img_bw->px[j].green, img->px[j].green);
     ck_assert_uint_eq(img_bw->px[j].blue, img->px[j].blue);
@@ -286,58 +308,59 @@ START_TEST(bw_example_image) {
   free(img->px);
   free(img_bw);
   free(img);
-
 }
 END_TEST
 
-START_TEST(bw_simple) {
+START_TEST(bw_simple)
+{
   struct image img;
   struct pixel pxl;
 
   img.size_x = img.size_y = 1;
   img.px = &pxl;
 
-  for(uint8_t i = 3; i < 5; i++) {
-    pxl.red = pxl.green = pxl.blue = i; 
-    pxl.alpha = 128;  
+  for (uint8_t i = 3; i < 5; i++)
+  {
+    pxl.red = pxl.green = pxl.blue = i;
+    pxl.alpha = 128;
 
-    uint8_t threshold = 4;  
-    filter_bw(&img, &threshold);  
+    uint8_t threshold = 4;
+    filter_bw(&img, &threshold);
     ck_assert_uint_eq(pxl.alpha, 128);
     ck_assert_uint_eq(pxl.red, pxl.green);
     ck_assert_uint_eq(pxl.red, pxl.blue);
-    ck_assert_uint_eq(pxl.red, (i > 4)? 255:0);
+    ck_assert_uint_eq(pxl.red, (i > 4) ? 255 : 0);
   }
 }
 END_TEST
 
-START_TEST(edge_threshold) {
+START_TEST(edge_threshold)
+{
   struct image img;
   struct pixel pxl;
   uint8_t threshold;
 
   img.size_x = img.size_y = 1;
   img.px = &pxl;
-  
+
   /* These calls should not crash */
   threshold = 0;
-  filter_bw(&img, &threshold);  
+  filter_bw(&img, &threshold);
   threshold = 255;
-  filter_bw(&img, &threshold);  
+  filter_bw(&img, &threshold);
 }
 END_TEST
 
 char *edge_deserts[] = {
-  "test_imgs/desert_edge_40.png",
-  "test_imgs/desert_edge_80.png",
-  "test_imgs/desert_edge_c0.png",
-  "test_imgs/desert_edge_e0.png"
-};
+    "test_imgs/desert_edge_40.png",
+    "test_imgs/desert_edge_80.png",
+    "test_imgs/desert_edge_c0.png",
+    "test_imgs/desert_edge_e0.png"};
 uint8_t edge_thresholds[] = {
-  0x40, 0x80, 0xc0, 0xe0
-};
+    0x40, 0x80, 0xc0, 0xe0};
 
-START_TEST(edge_example_image) {
+START_TEST(edge_example_image)
+{
   struct image *img, *img_edge, img_dup;
 
   ck_assert_int_eq(load_png("test_imgs/desert.png", &img), 0);
@@ -349,7 +372,8 @@ START_TEST(edge_example_image) {
 
   ck_assert_uint_eq(img_edge->size_x, img->size_x);
   ck_assert_uint_eq(img_edge->size_y, img->size_y);
-  for(long j = 0; j < img->size_x * img->size_y; j++) {
+  for (long j = 0; j < img->size_x * img->size_y; j++)
+  {
     ck_assert_uint_eq(img_edge->px[j].red, img->px[j].red);
     ck_assert_uint_eq(img_edge->px[j].green, img->px[j].green);
     ck_assert_uint_eq(img_edge->px[j].blue, img->px[j].blue);
@@ -360,11 +384,11 @@ START_TEST(edge_example_image) {
   free(img->px);
   free(img_edge);
   free(img);
-
 }
 END_TEST
 
-START_TEST(edge_checkerboard) {
+START_TEST(edge_checkerboard)
+{
   struct image *img, *img_edge, img_dup;
 
   ck_assert_int_eq(load_png("test_imgs/ck.png", &img), 0);
@@ -377,7 +401,8 @@ START_TEST(edge_checkerboard) {
 
   ck_assert_uint_eq(img_edge->size_x, img->size_x);
   ck_assert_uint_eq(img_edge->size_y, img->size_y);
-  for(long j = 0; j < img->size_x * img->size_y; j++) {
+  for (long j = 0; j < img->size_x * img->size_y; j++)
+  {
     ck_assert_uint_eq(img_edge->px[j].red, img->px[j].red);
     ck_assert_uint_eq(img_edge->px[j].green, img->px[j].green);
     ck_assert_uint_eq(img_edge->px[j].blue, img->px[j].blue);
@@ -388,22 +413,35 @@ START_TEST(edge_checkerboard) {
   free(img->px);
   free(img_edge);
   free(img);
-
 }
 END_TEST
 
-START_TEST(keying_limits) {
+START_TEST(keying_limits)
+{
   srand(time(NULL) ^ getpid());
 
   struct pixel key, pxls[9], pxl;
   struct image img;
 
   /* All pixels equal one randomly generated pixel */
-  do { pxl.red = rand();  } while(pxl.red == 0);
-  do { pxl.green = rand();  } while(pxl.green == 0);
-  do { pxl.blue = rand(); } while(pxl.blue == 0);
-  do { pxl.alpha = rand();  } while(pxl.alpha == 0);
-  for(unsigned i = 0; i < 9; i++) {
+  do
+  {
+    pxl.red = rand();
+  } while (pxl.red == 0);
+  do
+  {
+    pxl.green = rand();
+  } while (pxl.green == 0);
+  do
+  {
+    pxl.blue = rand();
+  } while (pxl.blue == 0);
+  do
+  {
+    pxl.alpha = rand();
+  } while (pxl.alpha == 0);
+  for (unsigned i = 0; i < 9; i++)
+  {
     pxls[i].red = pxl.red;
     pxls[i].green = pxl.green;
     pxls[i].blue = pxl.blue;
@@ -418,23 +456,26 @@ START_TEST(keying_limits) {
   key.green = 0;
   key.blue = 0;
   filter_keying(&img, &key);
-  for(unsigned i = 0; i < 9; i++) {
+  for (unsigned i = 0; i < 9; i++)
+  {
     ck_assert_uint_eq(pxls[i].red, pxl.red);
     ck_assert_uint_eq(pxls[i].green, pxl.green);
     ck_assert_uint_eq(pxls[i].blue, pxl.blue);
     ck_assert_uint_eq(pxls[i].alpha, pxl.alpha);
   }
-  
+
   /* Case 2: All pixels match, so should all be transparent */
   key = pxl;
   filter_keying(&img, &key);
-  for(unsigned i = 0; i < 9; i++) {
+  for (unsigned i = 0; i < 9; i++)
+  {
     ck_assert_uint_eq(pxls[i].alpha, 0);
   }
 }
 END_TEST
 
-START_TEST(keying_functionality) {
+START_TEST(keying_functionality)
+{
   srand(time(NULL) ^ getpid());
 
   struct image img = generate_rand_img();
@@ -444,27 +485,28 @@ START_TEST(keying_functionality) {
   key = img.px[rand() % (img.size_x * img.size_y)];
   filter_keying(&img, &key);
   long count_matches = 0;
-  for(long i = 0; i < img.size_x * img.size_y; i++) {
+  for (long i = 0; i < img.size_x * img.size_y; i++)
+  {
     struct pixel pxl = img.px[i], dup_pxl = img_dup.px[i];
-    if((dup_pxl.red == key.red) && (dup_pxl.green == key.green) &&
-       (dup_pxl.blue == key.blue)) {
+    if ((dup_pxl.red == key.red) && (dup_pxl.green == key.green) &&
+        (dup_pxl.blue == key.blue))
+    {
       ck_assert_uint_eq(pxl.alpha, 0);
       count_matches++;
-    }   
+    }
   }
   ck_assert_int_gt(count_matches, 0);
 }
 END_TEST
 
 char *grayscale_sources[] = {
-  "test_imgs/desert.png",
-  "test_imgs/summer.png"
-};
+    "test_imgs/desert.png",
+    "test_imgs/summer.png"};
 char *grayscale_output[] = {
-  "test_imgs/desert_gray.png",
-  "test_imgs/summer_gray.png"
-};
-START_TEST(grayscale_examples) {
+    "test_imgs/desert_gray.png",
+    "test_imgs/summer_gray.png"};
+START_TEST(grayscale_examples)
+{
   double weights[] = {0.2125, 0.7154, 0.0721};
   /* TODO: Implement */
 }
@@ -473,20 +515,23 @@ END_TEST
 /* Verify that the black image is inverted properly to a white image.
  * Then invert the result again and verify that you get a black image back
  * The alpha channel needs to be intact in both cases */
-START_TEST(negative_functionality) {
+START_TEST(negative_functionality)
+{
   /* TODO: Implement */
 }
 END_TEST
 
 /* Check if the filter doesn't crash when we pass a 0x0 image */
-START_TEST(negative_zero_size) {
+START_TEST(negative_zero_size)
+{
   /* TODO: Implement */
 }
 END_TEST
 
 /* Check for the simple, non-uniform, 3x3 test image that the blur filter
  * gives the correct output for different values of the radius (0, 1, 2, 3) */
-START_TEST(blur_functionality) {
+START_TEST(blur_functionality)
+{
   struct pixel black = {0, 0, 0, 255};
   struct pixel white = {252, 252, 252, 255};
   struct pixel px[3][3] = {{black, black, black}, {black, white, black}, {black, black, black}};
@@ -499,29 +544,32 @@ END_TEST
 /* Verify that the filter doesn't crash when we provide extreme values
  * for the radius (INT_MIN, INT_MAX, 0, image_width, image_height, all of the previous values divided by 2,
  * all of the previous values +- 1) */
-START_TEST(blur_radius_edge_cases) {
+START_TEST(blur_radius_edge_cases)
+{
   /* TODO: Implement */
 }
 END_TEST
 
 /* Verify for a random image that the transparency filter works properly */
-START_TEST(transparency_functionality) {
+START_TEST(transparency_functionality)
+{
   /* TODO: Implement */
 }
 END_TEST
 
 /* Check if the function crashes when we pass nullptr as the argument */
-START_TEST(transparency_edge_case) {
+START_TEST(transparency_edge_case)
+{
   /* TODO: Implement */
-
 }
 END_TEST
 
-int main() {
-  Suite* s = suite_create("lib-Y0l0 tests");
-  TCase *tc1 = tcase_create("edge case tests"); 
+int main()
+{
+  Suite *s = suite_create("lib-Y0l0 tests");
+  TCase *tc1 = tcase_create("edge case tests");
   suite_add_tcase(s, tc1);
-  TCase *tc2 = tcase_create("basic functionality tests"); 
+  TCase *tc2 = tcase_create("basic functionality tests");
   suite_add_tcase(s, tc2);
 
   /* TODO: Add more tests here */
@@ -552,11 +600,11 @@ int main() {
   tcase_add_test(tc2, edge_checkerboard);
   tcase_add_test(tc2, keying_functionality);
 
-  SRunner *sr = srunner_create(s); 
-  srunner_run_all(sr, CK_VERBOSE); 
+  SRunner *sr = srunner_create(s);
+  srunner_run_all(sr, CK_VERBOSE);
 
-  int number_failed = srunner_ntests_failed(sr); 
-  srunner_free(sr); 
+  int number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
 
-  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE; 
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
